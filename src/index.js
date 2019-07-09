c027f23a823c1578b3454a53cf53d43801bc99fa
TeemuHoyto <teemu.hoyto@gmail.com>
2019-07-09 06:57:04 +0300

    first up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+let hippobeba =document.getElementById('root');
+let marsuperse = <h1 class="stars">Hippojen bebat ovat suurepia kuin marsujen. This is true true</h1>
+ReactDOM.render(marsuperse, hippobeba);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
